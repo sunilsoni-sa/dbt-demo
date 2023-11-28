ef06b50e5150bf84d1e9ba6dae6e91ab021db0b4
Uttam Singh <uttams@sigmoidanalytics.com>
2023-11-28 19:00:41 +0530

    added sql

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -9,16 +9,9 @@
 
 {{ config(materialized='table') }}
 
-with source_data as (
-
-    select 1 as id
-    union all
-    select null as id
-
-)
 
 select *
-from source_data
+from demo_table
 
 /*
     Uncomment the line below to remove records with null `id` values
